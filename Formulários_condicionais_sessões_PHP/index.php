0a9bc253e268a3a76d8f62fb2c1b3fa99b81c770
Cassia <camsibr@gmail.com>
2021-02-07 18:58:35 -0300

    Método de requisição e resposta na web com PHP

---
+++ Formulários_condicionais_sessões_PHP/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário de Inscrição</title>
+</head>
+<body>
+
+<p>FORMULÁRIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php" method="post">
+    <p>Seu nome: <input type="text" name="nome"></p>
+    <p>Sua idade: <input type="text" name="idade"></p>
+    <p><input type="submit" value="Enviar dados do competidor"></p>
+</form>
+    
+</body>
+</html>
--- Formulários_condicionais_sessões_PHP/script.php
+++ Formulários_condicionais_sessões_PHP/script.php
@@ -1,1 +1,40 @@
 <?php
+
+$categorias = [];
+$categorias[] = 'infantil';
+$categorias[] = 'adoslecente';
+$categorias[] = 'adulto';
+$categorias[] = 'idoso';
+//print_r($categorias);
+
+$nome = $_POST['nome'];
+$idade = $_POST['idade'];
+
+var_dump($nome);
+var_dump($idade);
+return 0;
+
+if ($idade >= 6 && $idade <= 12)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if ($categorias[$i] == 'infantil')
+            echo "O nadador ",$nome, ", com idade ",$idade, " anos, compete na categoria infantil";
+    }
+}
+elseif ($idade >= 13 && $idade <= 18)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if ($categorias[$i] == 'adoslecente')
+            echo "O nadador ",$nome, ", com idade ",$idade, " anos, compete na categoria adoslecente";
+    }
+}
+else
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if ($categorias[$i] == 'adulto')
+            echo "O nadador ",$nome, ", com idade ",$idade, " anos, compete na categoria adulto";
+    }
+}
