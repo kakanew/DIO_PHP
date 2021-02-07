aabfbe2234d5febf2e9738b90929e1f44120cbae
Cassia <camsibr@gmail.com>
2021-02-07 19:42:48 -0300

    Validação do formulário com mensagens de erro

--- Formulários_condicionais_sessões_PHP/script.php
+++ Formulários_condicionais_sessões_PHP/script.php
@@ -12,21 +12,32 @@ $idade = $_POST['idade'];
 
 //var_dump($nome);
 //var_dump($idade);
-if (empty($nome))
+
+if (empty($nome)) // verifica se está vazio
 {
     echo 'O nome não pode ser vazio';
     return;
 }
-if (strlen($nome) < 3)
+
+if (strlen($nome) < 3) // verifica se é maior que 3 caracteres
 {
     echo 'O nome deve conter mais de 3 caracteres';
     return;
 }
-if (strlen($nome) > 40)
+
+if (strlen($nome) > 40) // verifica se for maior que 40 caracteres
 {
     echo 'O nome é muito extenso!';
     return;
 }
+
+if (!is_numeric($idade)) // verifica se é número
+{
+    echo 'Informe um número para idade';
+    return;
+}
+
+
 if ($idade >= 6 && $idade <= 12)
 {
     for ($i = 0; $i <= count($categorias); $i++)
@@ -39,7 +50,7 @@ elseif ($idade >= 13 && $idade <= 18)
 {
     for ($i = 0; $i <= count($categorias); $i++)
     {
-        if ($categorias[$i] == 'adoslecente')
+        if ($categorias[$i] == 'adolescente')
             echo "O nadador ",$nome, ", com idade ",$idade, " anos, compete na categoria adoslecente";
     }
 }
