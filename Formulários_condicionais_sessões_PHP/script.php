985a0df4d5e9562675f6757cabc84c2d95da3a81
Cassia <camsibr@gmail.com>
2021-02-08 15:02:44 -0300

    Operador ternário para validar os dados da sessão

--- Formulários_condicionais_sessões_PHP/script.php
+++ Formulários_condicionais_sessões_PHP/script.php
@@ -6,6 +6,7 @@ $categorias[] = 'infantil';
 $categorias[] = 'adoslecente';
 $categorias[] = 'adulto';
 $categorias[] = 'idoso';
+$categorias[] = 'bebe';
 //print_r($categorias);
 
 $nome = $_POST['nome'];
@@ -16,26 +17,26 @@ $idade = $_POST['idade'];
 
 if (empty($nome)) // verifica se está vazio
 {
-    $_SESSION[''] 'O nome não pode ser vazio';
-    return;
+    $_SESSION['mensagem de erro'] = 'O nome não pode ser vazio, por favor preencha novamente.';
+    header('location: index.php');
 }
 
 if (strlen($nome) < 3) // verifica se é maior que 3 caracteres
 {
-    echo 'O nome deve conter mais de 3 caracteres';
-    return;
+    $_SESSION['mensagem de erro'] = 'O nome deve conter mais de 3 caracteres.';
+    header('location: index.php');
 }
 
 if (strlen($nome) > 40) // verifica se for maior que 40 caracteres
 {
-    echo 'O nome é muito extenso!';
-    return;
+    $_SESSION['mensagem de erro'] = 'O nome é muito extenso! Limite 40 caradteres.';
+    header('location: index.php');
 }
 
 if (!is_numeric($idade)) // verifica se é número
 {
-    echo 'Informe um número para idade';
-    return;
+    $_SESSION['mensagem de erro'] = 'A idade deve ser preenchida com números';
+    header('location: index.php');
 }
 
 
@@ -44,7 +45,7 @@ if ($idade >= 6 && $idade <= 12)
     for ($i = 0; $i <= count($categorias); $i++)
     {
         if ($categorias[$i] == 'infantil')
-            echo "O nadador ",$nome, ", com idade ",$idade, " anos, compete na categoria infantil";
+            echo 'O nadador ',$nome, ', com idade ',$idade, ' anos, compete na categoria infantil';
     }
 }
 elseif ($idade >= 13 && $idade <= 18)
@@ -52,14 +53,22 @@ elseif ($idade >= 13 && $idade <= 18)
     for ($i = 0; $i <= count($categorias); $i++)
     {
         if ($categorias[$i] == 'adolescente')
-            echo "O nadador ",$nome, ", com idade ",$idade, " anos, compete na categoria adoslecente";
+            echo 'O nadador ',$nome, ', com idade ',$idade, ' anos, compete na categoria adoslecente';
+    }
+}
+elseif ($idade >= 18 && $idade <= 130)
+{
+    for ($i = 0; $i <= count($categorias); $i++)
+    {
+        if ($categorias[$i] == 'adulto')
+            echo 'O nadador ',$nome, ', com idade ',$idade, ' anos, compete na categoria adulto';
     }
 }
 else
 {
     for ($i = 0; $i <= count($categorias); $i++)
     {
-        if ($categorias[$i] == 'adulto')
-            echo "O nadador ",$nome, ", com idade ",$idade, " anos, compete na categoria adulto";
+        if ($categorias[$i] == 'bebe')
+            echo 'O nadador ',$nome, ', com idade ',$idade, ' anos, não pode competir pois é um bebê.';
     }
 }
